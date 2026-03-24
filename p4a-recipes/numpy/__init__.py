27b478e7d700d1e548b5de5ccb09b8399f8e9eb3
Juhan Kim <kjhan0606@gmail.com>
2026-03-25 05:56:31 +0900

    fix: rewrite custom numpy recipe to use MesonRecipe (compatible with current p4a)
    The old recipe tried to import NumPyRecipe from p4a's numpy recipe,
    but current p4a renamed it to NumpyRecipe and switched to MesonRecipe.
    Now using a standalone MesonRecipe-based recipe for numpy 1.26.4.

--- p4a-recipes/numpy/__init__.py
+++ p4a-recipes/numpy/__init__.py
@@ -1,9 +1,44 @@
-from pythonforandroid.recipes.numpy import NumPyRecipe
+from pythonforandroid.recipe import MesonRecipe, Recipe
+from os.path import join
+import shutil
 
 
-class NumPyRecipe126(NumPyRecipe):
+class NumpyRecipe(MesonRecipe):
     version = '1.26.4'
     url = 'https://github.com/numpy/numpy/releases/download/v{version}/numpy-{version}.tar.gz'
+    hostpython_prerequisites = ["Cython>=3.0.6", "numpy==1.26.4"]
+    extra_build_args = ['-Csetup-args=-Dblas=none', '-Csetup-args=-Dlapack=none']
+    need_stl_shared = True
+    min_ndk_api_support = 24
+
+    def get_recipe_meson_options(self, arch):
+        options = super().get_recipe_meson_options(arch)
+        options["binaries"]["python"] = self.ctx.python_recipe.python_exe
+        options["binaries"]["python3"] = self.ctx.python_recipe.python_exe
+        options["properties"]["longdouble_format"] = (
+            "IEEE_DOUBLE_LE" if arch.arch in ["armeabi-v7a", "x86"]
+            else "IEEE_QUAD_LE"
+        )
+        return options
+
+    def get_recipe_env(self, arch, **kwargs):
+        env = super().get_recipe_env(arch, **kwargs)
+        env["_PYTHON_HOST_PLATFORM"] = arch.command_prefix
+        env["NPY_DISABLE_SVML"] = "1"
+        env["TARGET_PYTHON_EXE"] = join(
+            Recipe.get_recipe("python3", self.ctx).get_build_dir(arch.arch),
+            "android-build", "python"
+        )
+        return env
+
+    def build_arch(self, arch):
+        super().build_arch(arch)
+        self.restore_hostpython_prerequisites(["cython"])
+
+    def get_hostrecipe_env(self, arch=None):
+        env = super().get_hostrecipe_env(arch=arch)
+        env['RANLIB'] = shutil.which('ranlib')
+        return env
 
 
-recipe = NumPyRecipe126()
+recipe = NumpyRecipe()
